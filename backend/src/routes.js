e2f0824311e40f63863933f556fe92627017a1ae
jotasimione <j.simione@users.noreply.github.com>
2019-11-27 00:57:08 -0300

    Criado rotas All e ById

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 
 import FaseController from './controllers/FaseController';
-// import JogoController from './controllers/JogoController';
-// import UsuarioController from './controllers/UsuarioController';
+import JogoController from './controllers/JogoController';
+import UsuarioController from './controllers/UsuarioController';
+import MovimentoController from './controllers/MovimentoController';
 
 const routes = new Router();
 
-routes.get('/allfases', FaseController.allFases);
-// routes.get('/users', JogoController.update);
-// routes.get('/users', UsuarioController.update);
+routes.get('/allfases', FaseController.getAll);
+routes.get('/alljogos', JogoController.getAll);
+routes.get('/allusuarios', UsuarioController.getAll);
+routes.get('/allmovimentos', MovimentoController.getAll);
+
+routes.get('/fase:id', FaseController.getById);
+routes.get('/jogo:id', JogoController.getById);
+routes.get('/usuario:id', UsuarioController.getById);
+routes.get('/movimento:id', MovimentoController.getById);
 
 export default routes;
