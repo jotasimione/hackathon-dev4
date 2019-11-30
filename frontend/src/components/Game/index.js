cc05a80f68045ede1b54702906dbbbe75452beb9
anamsousa <57605555+anamsousa@users.noreply.github.com>
2019-11-30 07:37:24 -0300

    alteraçõezinhas

--- frontend/src/components/Game/index.js
+++ frontend/src/components/Game/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "../Main/Main-Styled.js";
 import { Link } from "react-router-dom";
 import imgVoltar from "../../assets/arrow-left.png";
+import imgChat from "../../assets/chat.png";
 
 export default function Game() {
   return (
@@ -12,7 +13,7 @@ export default function Game() {
             <img src={imgVoltar} alt="" className="Imagem" />
           </Link>
         </div>
-        <div className="Right">
+        <div className="Center">
           Game: {window.location.pathname.replace("/Game/", "")}
         </div>
         <div style={{ clear: "both" }}></div>
@@ -20,10 +21,24 @@ export default function Game() {
 
       <div className="Corpo">
         <div className="Linha">
-          Aeww
-          <div style={{ clear: "both" }}></div>
+          <p>
+            Em uma tarde de sábado ensolarada, João estava em sua casa passando
+            um tempo, e estava faminto, então resolveu ir até a lanchonete do
+            centro da cidade para comer o seu lanche preferido.​
+          </p>
         </div>
       </div>
+      <div className="Corpo2">
+        <div className="Linha">
+          <p>
+            Selecione a ordem correta das ações para o João chegar ao
+            Restaurante
+          </p>
+          <img src={imgChat} alt="" className="ImagemChat" />
+        </div>
+      </div>
+
+      <div style={{ clear: "both" }}></div>
 
       <div className="Rodape" align="center">
         Confirmar
